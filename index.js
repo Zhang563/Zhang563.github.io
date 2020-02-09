80de30693c2a501b9aaf624826578daa0aa238e2
Zhang563 <60804083+Zhang563@users.noreply.github.com>
2020-02-09 11:40:06 -0500

    WORKING

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 function getRandomFact(){
-  $.get('http://numbersapi.com/1', function(data) {
-    $('#numberfact').text(data);
-    console.log($('#numberfact').text(data));
+const n = document.querySelector("#innumber").value;
+  $.get( `http://numbersapi.com/${n}`, function(data) {
+    $('#number').text(data);
+    console.log($('#number').text(data));
+
   });
+
 }
-// function getRandomDog(){
-//       fetch("https://dog.ceo/api/breeds/image/random")
-//           .then(res => res.json())
-//           .then(function(data) {
-//               console.log(data.message);
-//               document.querySelector("#dog-image").style.backgroundImage=`url(${data.message})`;
-//           });
-//
-//   }
