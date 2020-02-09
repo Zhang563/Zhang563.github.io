47be0d547b1ab03ccdb744535ab88ee41de93ccb
Zhang563 <60804083+Zhang563@users.noreply.github.com>
2020-02-09 13:54:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const n = document.querySelector("#innumber").value;
 }
 const element =  document.querySelector('.body-content');
 element.classList.add('animated', 'bounceIn');
+const element1= document.querySelector('.title')
+element.classList.add('animated','bounceInUp')
+
+
+document.querySelector("#innumber").addEventListener("keydown", myNewFunction);
+function myNewFunction(event) {
+  if(event.keyCode===13){
+    getRandomFact()
+  
+}
