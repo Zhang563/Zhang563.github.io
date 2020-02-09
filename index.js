4ffd89f4e456482e33700d6aa8114740e7dda4b6
Zhang563 <60804083+Zhang563@users.noreply.github.com>
2020-02-09 12:02:17 -0500

    update

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 function getRandomFact(){
 const n = document.querySelector("#innumber").value;
-  $.get( `http://numbersapi.com/${n}`, function(data) {
+  $.get(`http://numbersapi.com/${n}`, function(data) {
     $('#number').text(data);
-    // if (data === error){
-    //   alert("Pick a number");
-    // }else {
     console.log($('#number').text(data));
-    // }
-
   });
 
 }
